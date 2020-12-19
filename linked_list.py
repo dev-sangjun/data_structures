99f59631d9082ae8a4fc6b3960489bde92529502
devsangjun <33212292+devsangjun@users.noreply.github.com>
2020-12-19 15:39:20 -0800

    linked_list

---
+++ linked_list.py
@@ -0,0 +1,62 @@
+class IndexOutOfRangeError(Exception):
+  def __init__(self, message):
+    self.message = message
+  def __str__(self):
+    return self.message
+
+class Node:
+  def __init__(self, element = None):
+    self.element = element
+    self.next = None
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.length = 0
+
+  def append(self, element):
+    node = Node(element)
+    if self.head == None:
+      # the list is empty
+      self.head = node
+    else:
+      cur = self.head
+      while cur.next != None:
+        cur = cur.next
+      cur.next = node
+    self.length += 1
+
+  def get(self, index):
+    if index >= self.length or index < 0:
+      raise IndexOutOfRangeError("Index is out of range")
+    else:
+      cur = self.head
+      for _ in range(index):
+        cur = cur.next
+      return cur.element
+
+  def remove(self, index):
+    if index >= self.length or index < 0:
+      raise IndexOutOfRangeError("Index is out of range")
+    else:
+      cur = self.head
+      if index == 0:
+        self.head = cur.next
+      else:
+        prev_node = None
+        for _ in range(index):
+          prev_node = cur
+          cur = cur.next
+        prev_node.next = cur.next
+
+  def print(self):
+    elements = []
+    cur = self.head
+    while cur != None:
+      elements.append(cur.element)
+      cur = cur.next
+    print(elements)
+
+# test code
+linked_list = LinkedList()
+
